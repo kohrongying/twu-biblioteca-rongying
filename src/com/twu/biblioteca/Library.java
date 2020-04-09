22c11d24eba5bc52616e85e45072e16b11210e24
Rong Ying <kohrongying@gmail.com>
2020-04-09 11:12:34 +0800

    Add Library and Test methods for returning and borrowing a book

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,9 +1,12 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Library {
     private Book[] books;
+    private List<BookLoan> loans = new ArrayList<BookLoan>();
 
     public Library(Book[] books) {
         this.books = books;
@@ -15,5 +18,31 @@ public class Library {
                 .toArray(Book[]::new);
     }
 
+    public BookLoan[] getOutstandingLoans() {
+        return this.loans.stream()
+                .filter(loan -> loan.isOutstanding())
+                .toArray(BookLoan[]::new);
+    }
 
+    public BookLoan checkoutBook(Book book) {
+        if (book.isAvailable()) {
+            BookLoan loan = new BookLoan(book);
+            this.loans.add(loan);
+            System.out.println(Messages.BOOK_CHECKOUT_SUCCESS.getMessage());
+            return loan;
+        } else {
+            System.out.println(Messages.BOOK_CHECKOUT_FAIL.getMessage());
+            throw new IllegalBookCheckoutException(Messages.BOOK_CHECKOUT_FAIL.getMessage());
+        }
+    }
+
+    public void returnLoan(BookLoan loan) {
+        if (loan.isOutstanding()) {
+            loan.returnBook();
+            System.out.println(Messages.BOOK_RETURN_SUCCESS.getMessage());
+        } else {
+            System.out.println(Messages.BOOK_RETURN_FAIL.getMessage());
+            throw new IllegalBookReturnException(Messages.BOOK_RETURN_FAIL.getMessage());
+        }
+    }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
 
@@ -7,14 +8,53 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class LibraryTest {
+    private Book book;
+    private Book book2;
+    private Book book3;
+    private Book[] books;
+    private Library library;
+
+    @Before
+    public void setUp() {
+        book = new Book("TDD for Dummies", "TWU", 2000, 1, 1);
+        book2 = new Book("The Agile Brown Fox Jumped Over the Waterfall", "Marty Howler", 2010, 1, 1);
+        book3 = new Book("Pear Programming", "Fruity Peeler", 2012, 1, 0);
+        books = new Book[]{book, book2, book3};
+        library = new Library(books);
+    }
 
     @Test
-    public void shouldGetListOfAvailableBooksWhenDisplayingListOfBooks() {
-        Book book = new Book("TDD for Dummies", "TWU", 2000, 1, 1);
-        Book book2 = new Book("The Agile Brown Fox Jumped Over the Waterfall", "Bartin Howler", 2010, 1, 1);
-        Book book3 = new Book("Pear Programming", "Fruity Peeler", 2012, 1, 0);
-        Book[] books = new Book[]{book, book2, book3};
-        Library library = new Library(books);
+    public void shouldGetListOfAvailableBooks() {
         assertThat(library.getAvailableBooks(), is(new Book[]{book, book2}));
+    }
+
+    @Test
+    public void shouldGetListOfOutstandingLoans() {
+        assertThat(library.getOutstandingLoans().length, is(0));
+    }
+
+    @Test
+    public void shouldCreateBookLoanWhenCheckingOutBook() {
+        BookLoan loan = library.checkoutBook(book);
+        assertThat(library.getOutstandingLoans(), is(new BookLoan[]{loan}));
+    }
+
+    @Test(expected = IllegalBookCheckoutException.class)
+    public void shouldThrowErrorWhenCheckingOutUnavailableBook() {
+        library.checkoutBook(book3);
+    }
+
+    @Test
+    public void shouldRemoveBookFromOutstandingLoansAfterReturningBook() {
+        BookLoan loan = library.checkoutBook(book);
+        library.returnLoan(loan);
+        assertThat(library.getOutstandingLoans().length, is(0));
+    }
+
+    @Test(expected = IllegalBookReturnException.class)
+    public void shouldThrowErrorWhenReturningInvalidBook() {
+        BookLoan loan = library.checkoutBook(book);
+        library.returnLoan(loan);
+        library.returnLoan(loan);
     }
 }
